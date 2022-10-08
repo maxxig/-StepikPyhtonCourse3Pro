a7cb1a0092d6b9d92764d6d70501f42210e72d9f
Maks Golovin <maxxig@gmail.com>
2022-10-08 06:25:46 +0300

    save

---
+++ 4.2/6.py
@@ -0,0 +1,19 @@
+import csv
+data, data_dict, columns = [], {}, ['domain','count']
+with open('data.csv', encoding='utf-8') as file:
+    data = [d[0].split(',')[2] for d in csv.reader(file, delimiter=';')]
+
+del data[0]
+
+data = list(map(lambda x: x.split('@')[1], data))
+for d in data:
+    if d not in data_dict:
+        data_dict[d] = 1
+    else:
+        data_dict[d] += 1
+data_dict = [list(s) for s in sorted(data_dict.items(), key=lambda x: (x[1],x[0]))]
+
+with open('domain_usage.csv', 'w',  encoding='utf-8', newline='') as file:
+    write = csv.writer(file)
+    write.writerow(columns)
+    write.writerows(data_dict)
