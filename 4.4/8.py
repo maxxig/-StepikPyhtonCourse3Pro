e183317ad421e5c91e4fefc81e07a1e6bc9fd17f
Maks Golovin <maxxig@gmail.com>
2022-10-14 21:38:18 +0300

    save

---
+++ 4.4/8.py
@@ -0,0 +1,12 @@
+import json
+
+with open('data1.json', encoding='utf-8') as file1:
+    data1 = json.load(file1)
+
+with open('data2.json', encoding='utf-8') as file2:
+    data2 = json.load(file2)
+
+data1.update(data2)
+
+with open('data_merge.json','w') as output_file:
+    json.dump(data1, output_file, indent='   ')
---
+++ 4.4/9.py
@@ -0,0 +1,20 @@
+import json
+
+with open('people.json', encoding='utf-8') as input_file:
+    data = json.load(input_file)
+
+my_set = set()
+for d in data:
+    my_set.update(d.keys())
+
+
+#my_dict = {s: None for s in my_set} var 1
+
+my_dict = dict.fromkeys(my_set, None)
+for i, d in enumerate(data):
+    append_result = my_dict.copy()
+    append_result.update(d)
+    data[i] = append_result
+
+with open('updated_people.json','w') as output_file:
+    json.dump(data, output_file, indent='   ')
