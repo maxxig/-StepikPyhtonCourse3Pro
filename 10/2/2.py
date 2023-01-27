ad76e1d380408ac68c8e1e054019f6609c34b01d
Maks Golovin <maxxig@gmail.com>
2023-01-27 14:03:05 +0300

    everyday

---
+++ 10/2/2.py
@@ -0,0 +1,14 @@
+def transpose(m):
+    return [list(z) for z in zip(*matrix)]
+
+
+
+matrix = [['1', '2'],
+          ['4', '5'],
+          ['7', '8'],
+          ['3', 4],
+          [True, None],
+          [9, 80],
+          [1, -1]]
+
+print(transpose(matrix))
